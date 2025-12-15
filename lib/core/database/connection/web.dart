28b7ba7ce7fc3a7fc616c57b08957538f2d9c5bd
wbruck <brucknerw@gmail.com>
2025-12-14 21:44:07 -0600

    Fix WebDatabase initializer - remove empty async function

--- lib/core/database/connection/web.dart
+++ lib/core/database/connection/web.dart
@@ -3,10 +3,5 @@ import 'package:drift/web.dart';
 
 /// Opens a database connection for web platform using sql.js + IndexedDB.
 QueryExecutor openConnection() {
-  return WebDatabase(
-    'common_place_book_db',
-    initializer: () async {
-      // sql.js files are loaded automatically from web/ directory
-    },
-  );
+  return WebDatabase('common_place_book_db');
 }
